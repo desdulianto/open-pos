1ce522d941fabfe2016fc23c1e2d7a13554e1d21
Maikel Chandika <mkdika@gmail.com>
2018-02-05 08:12:53 +0700

    add backend project structure & its dependencies - Close #4

---
+++ open-pos-backend/src/main/java/com/desdulianto/openposbackend/OpenPosBackendApplication.java
@@ -0,0 +1,12 @@
+package com.desdulianto.openposbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenPosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenPosBackendApplication.class, args);
+	}
+}
