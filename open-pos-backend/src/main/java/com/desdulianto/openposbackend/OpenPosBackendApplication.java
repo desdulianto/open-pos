99497991c540f40ae5b281e0e15ea312b4377626
Maikel Chandika <mkdika@gmail.com>
2018-02-05 08:24:44 +0700

    refine back-end project skeleton

--- open-pos-backend/src/main/java/com/desdulianto/openposbackend/OpenPosBackendApplication.java
+++ open-pos-backend/src/main/java/com/desdulianto/openposbackend/OpenPosBackendApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OpenPosBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(OpenPosBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(OpenPosBackendApplication.class, args);
+    }
 }
